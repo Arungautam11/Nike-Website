f0d70d9dbfda5a71a23db3123ca887dcda394f2d
ArunGautam <asgautam19@gmail.com>
2024-09-27 16:09:08 +0530

    complete nav section of website

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,4 +1,4 @@
-// import { hamburger } from "../assets/icons";
+import { hamburger } from "../assets/icons";
 import { headerLogo } from "../assets/images";
 import { navLinks } from "../constants";
 
@@ -26,6 +26,14 @@ const Nav = () => {
                     </li>
                 ))}
             </ul>
+            <div className="flex gap-2 text-lg leading-normal font-medium font-montserrat max-lg:hidden wide:mr-24">
+                <a href='/'>Sign in</a>
+                <span>/</span>
+                <a href='/'>Explore now</a>
+            </div>
+            <div className="hidden max-lg:block">
+                <img src={hamburger} alt='hamburger icon' width={25} height={25} />
+            </div>
         </nav>
     </header>
   )
